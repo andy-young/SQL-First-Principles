d6f3e1c61ae3b36e9526af694d5891bad59f51f3
Andy Young <ayscript@gmail.com>
2018-10-29 16:21:03 -0600

    Watch video, make changes..

---
+++ ex04_Insert_Referential_Data/ex3.sql
@@ -0,0 +1,7 @@
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (0, 'Andy', 'Young', 38);
+
+INSERT INTO pet (id, name, breed, age, dead)
+  VALUES (0, 'Vony', 'Domestic Cat', 5, 0);
+
+INSERT INTO pet VALUES (1, 'Miso', 'Russian Blue', 3, 0);
